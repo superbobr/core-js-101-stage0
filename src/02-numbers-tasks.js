cc4722b8b58ac4c1a1f039840ee697779ebee184
Leonid Seoshnik <leonidseoshnik@MacBook-Air-Leonid.local>
2025-08-08 11:30:20 +0300

    add two solutions

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -122,8 +122,9 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  const result = value.toString().split('');
+  return result[result.length - 1];
 }
 
 /**
@@ -137,8 +138,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return +value;
 }
 
 /**
