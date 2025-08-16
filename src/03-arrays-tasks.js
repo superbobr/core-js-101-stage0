d7eca947acefe67edcc04f86970e45905c818331
Leonid Seoshnik <leonidseoshnik@MacBook-Air-Leonid.local>
2025-08-16 20:07:14 +0300

    fix arrays tasks and add solution

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -127,7 +127,7 @@ function getUpperCaseStrings(arr) {
  *    [ 'angular', 'react', 'ember' ] => [ 7, 5, 5 ]
  */
 function getStringsLength(arr) {
-  arr.map((item) => item.length);
+  return arr.map((item) => item.length);
 }
 
 /**
@@ -193,8 +193,8 @@ function getTail(arr, n) {
  *    +'20,21,22,23,24\n'
  *    +'30,31,32,33,34'
  */
-function toCsvText(/* arr */) {
-  throw new Error('Not implemented');
+function toCsvText(arr) {
+  return arr.map((row) => row.join(',')).join('\n');
 }
 
 /**
@@ -268,8 +268,10 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return arr.flatMap((item, index) => {
+    return Array(index + 1).fill(item);
+  });
 }
 
 /**
