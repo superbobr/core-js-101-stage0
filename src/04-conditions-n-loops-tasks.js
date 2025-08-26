6d9c73a41e47253625ed6a5391408bf9b862a05c
Leonid Seoshnik <leonidseoshnik@MacBook-Air-Leonid.local>
2025-08-26 12:26:08 +0300

    add solutions

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -122,8 +122,13 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  const noOverlap =
+    rect1.left + rect1.width <= rect2.left || // rect1 слева
+    rect1.left >= rect2.left + rect2.width || // rect1 справа
+    rect1.top + rect1.height <= rect2.top || // rect1 выше
+    rect1.top >= rect2.top + rect2.height; // rect1 ниже
+  return !noOverlap;
 }
 
 /**
@@ -152,10 +157,14 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
-}
+function isInsideCircle(circle, point) {
+  const { center, radius } = circle;
+  const dx = point.x - center.x;
+  const dy = point.y - center.y;
+  const distanceSquared = dx * dx + dy * dy;
 
+  return distanceSquared < radius * radius;
+}
 /**
  * Returns the first non repeated char in the specified strings otherwise returns null.
  *
